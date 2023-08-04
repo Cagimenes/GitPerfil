7966c2fdf56a469a9731ec00d0e7e0cec6a0deb1
Cagimenes <carolmicagimenes@gmail.com>
2023-08-04 10:08:07 -0300

    Alteração de cor e ajustes

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+
+function trocaCor() 
+{
+    document.body.style.backgroundColor = 'darkviolet';
+    document.getElementsByTagName("main")[0].style.backgroundColor = 'mediumpurple';
+    document.getElementsByClassName("sobre-mim")[0].style.backgroundColor = 'darkorchid';
+    
+}
