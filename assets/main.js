d1379f963cf7f6d558a4948460c60ae1c79931f4
Cagimenes <carolmicagimenes@gmail.com>
2023-08-04 11:12:59 -0300

    Termino de ajustes

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,7 @@
 
 function trocaCor() 
 {
-    document.body.style.backgroundColor = 'darkviolet';
+    document.body.style.backgroundColor = 'lavender';
     document.getElementsByTagName("main")[0].style.backgroundColor = 'mediumpurple';
-    document.getElementsByClassName("sobre-mim")[0].style.backgroundColor = 'darkorchid';
-    
+    document.getElementsByClassName("sobre-mim")[0].style.borderColor  = 'darkorchid';
 }
